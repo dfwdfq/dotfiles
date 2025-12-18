feaa95357827250925ba41dca3f2451905dffa98
uncle_joe <uncle_joe@kali.kali.com>
2025-12-18 10:19:18 +0300

    main dotfile

---
+++ emacs/init.el
@@ -0,0 +1,80 @@
+;; Disable the menu bar
+(menu-bar-mode -1)
+
+(global-display-line-numbers-mode 1)
+
+;; Global font lock
+(global-font-lock-mode t)
+
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Add Melpa to package archives
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(defun open-config ()
+  (interactive)
+  (find-file "~/.config/emacs/init.el"))
+
+
+(add-to-list 'custom-theme-load-path "~/.config/emacs/themes/")
+(load-theme 'deep-purple t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(auto-complete markdown-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+;; Define a macro to move to the beginning of the buffer
+(fset 'move-to-beginning-of-buffer
+   (lambda () "Move to the beginning of the buffer" (interactive)
+     (goto-char (point-min))))
+
+;; Define a macro to move to the end of the buffer
+(fset 'move-to-end-of-buffer
+   (lambda () "Move to the end of the buffer" (interactive)
+     (goto-char (point-max))))
+
+;; Bind C-x-a to move to the beginning of the buffer
+(global-set-key (kbd "C-x a") 'move-to-beginning-of-buffer)
+
+;; Bind C-x-e to move to the end of the buffer
+(global-set-key (kbd "C-x e") 'move-to-end-of-buffer)
+
+(defun verse-insert ()
+  "Insert verse in org mode"
+  (interactive)
+  (insert "#+BEGIN_VERSE\n\n#+END_VERSE")
+  (forward-line -1))
+
+(require 'org)
+(define-key org-mode-map (kbd "M-v v") #'verse-insert)
+
+
+(add-to-list 'load-path "~/.config/emacs/lisp")  
+(require 'welcome-buffer)
+
+;; Display welcome buffer on startup
+(add-hook 'emacs-startup-hook #'display-welcome-buffer)
+
+(setq inhibit-startup-screen t)
+
+(set-face-attribute 'default nil :height 210)
+(tool-bar-mode -1)
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
