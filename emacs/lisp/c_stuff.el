d1b03ecbe2ac8e229cd722cd6ac35f99136daf5c
uncle_joe <uncle_joe@kali.kali.com>
2026-01-06 21:42:47 +0300

    this module defines functions for working with c files

---
+++ emacs/lisp/c_stuff.el
@@ -0,0 +1,17 @@
+(defun create-source-header ()
+  "Create header file and associated source file."
+  (interactive)
+  (let* ((filename (read-string "Enter file name (no .h): "))
+         (c-file (concat filename ".c"))
+         (h-file (concat filename ".h"))
+         (guard-var (concat (upcase filename) "_H")))
+    
+    (find-file c-file)
+    (insert "#include \"" h-file "\"\n\n")
+    (save-buffer)
+    
+    (find-file h-file)
+    (insert "#ifndef " guard-var "\n")
+    (insert "#define " guard-var "\n\n")
+    (insert "#endif /* " guard-var " */\n")
+    (save-buffer)))
