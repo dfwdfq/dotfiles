69ae0b38e01f8b2b15a3d954316b9dcdb733df86
uncle_joe <uncle_joe@kali.kali.com>
2026-01-22 20:25:23 +0300

    added function to insert source code within org note

--- emacs/init.el
+++ emacs/init.el
@@ -62,8 +62,15 @@
   (insert "#+BEGIN_VERSE\n\n#+END_VERSE")
   (forward-line -1))
 
+(defun source-insert ()
+  "Insert source in org mode"
+  (interactive)
+  (insert "#+BEGIN_SRC\n\n#+END_SRC")
+  (forward-line -2))
+
 (require 'org)
 (define-key org-mode-map (kbd "M-v v") #'verse-insert)
+(define-key org-mode-map (kbd "M-v c") #'source-insert)
 
 
 (add-to-list 'load-path "~/.config/emacs/lisp")  
