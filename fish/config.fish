f2b523a92904ec5b8b492f9aa69c6821541b99a0
uncle_joe <uncle_joe@kali.kali.com>
2026-02-25 06:12:03 +0300

    added some variables and commented them

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,19 @@
+#variable name:VISUAL
+#purpose: whenever some programs needs editor they look for this variable
+#value: emacs -nw
+#.
+set -gx VISUAL  "emacs -nw"
+#variable name:EDITOR
+#purpose: same as $VISUAL
+#value: emacs -nw
+#.
+set -gx EDITOR  "emacs -nw"
+#variable name:BROWSER
+#purpose: look for this variable when it's required to open browser
+#value: firefox
+#.
+set -gx BROWSER firefox
+
 if status is-interactive
     fortune | cowsay
 end
