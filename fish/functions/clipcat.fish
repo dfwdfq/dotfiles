e1b2af25b479cd9828e6b81e853340ee68011e27
uncle_joe <uncle_joe@kali.kali.com>
2026-02-25 03:55:44 +0300

    added documentation for later parsing

--- fish/functions/clipcat.fish
+++ fish/functions/clipcat.fish
@@ -1,3 +1,6 @@
+#function name: clipcat
+#arguments    : any
+#purpose      : put passed files into X clipboard.
 function clipcat
    cat $argv | xclip -i
 end
--- fish/functions/tell.fish
+++ fish/functions/tell.fish
@@ -1,3 +1,6 @@
+#function name: tell
+#arguments    : none
+#purpose      : print to STDOUT content of X clipboard.
 function tell
    xclip -o
 end
