c814f9f21c6f0a191cae0d7a8bf151d5b2cc7285
uncle_joe <uncle_joe@kali.kali.com>
2026-02-25 04:31:33 +0300

    wrote first script that parses fish functions and generates org file as documentation

---
+++ docs/scripts/parse_fish_functions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+
+ff_dir = os.path.expanduser("~/.config/fish/functions/")
+functions = (ff_dir+x for x in os.listdir(ff_dir))
+
+def read_first_3(path):
+    with open(path,"r") as f:
+        head = [next(f) for _ in range(3)]
+    return head
+def parse(lst):
+    name, arguments, purpose = lst
+    name = name.replace("#function name:","").strip()
+    arguments = arguments.replace("#arguments:","").strip()    
+    purpose = purpose.replace("#purpose:","").strip()
+    return name,purpose,arguments
+
+def generate_entry(name, purpose, arguments):
+    entry ="""
+* ~{}~ function
+** purpose
+#+BEGIN_VERSE
+{}
+#+END_VERSE
+** arguments
+#+BEGIN_VERSE
+{}
+#+END_VERSE
+    """
+    return entry.format(name,purpose,arguments)
+
+output = ["#+title: fish functions"]
+for f in functions:
+    name,purpose,arguments = parse(read_first_3(f))
+    output.append(generate_entry(name,purpose,arguments))
+    
+with open("../fish_functions.org","w") as f:
+    f.write("\n".join(output))
