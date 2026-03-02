ff94211ae01ab3537ea0e85ae8487504863caa86
hdvdrm <haskel.list@yandex.ru>
2026-03-02 18:15:30 +0300

    push manually

--- fish/functions/gendoc.fish
+++ fish/functions/gendoc.fish
@@ -3,10 +3,6 @@
 #purpose: re-generate all documentation for dotfiles
 function gendoc
    python ~/.config/docs/scripts/run_all.py
-   set -l here (pwd)
    cd ~/.config
-   git add docs/emacs-docs/* docs/kitty-docs/* docs/fish-docs/* docs/qtile-docs/*
-   git commit -m"regenerated docs."
-   gpush dotfiles master
-   cd $here
+   git status
 end
