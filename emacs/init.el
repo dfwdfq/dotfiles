b654449f024f721e1a891f1dfa955969815459d9
uncle_joe <uncle_joe@kali.kali.com>
2026-05-19 17:58:23 +0300

    make bold in org look better

--- emacs/init.el
+++ emacs/init.el
@@ -25,7 +25,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company auto-complete corfu racket-mode)))
+ '(package-selected-packages '(rust-mode company auto-complete corfu racket-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -112,3 +112,9 @@
 
   :hook
   (after-init . global-company-mode))
+
+(setq org-hide-emphasis-markers t)
+
+(use-package rust-mode
+  :ensure t
+  :mode ("\\.rs\\'" . rust-mode))
