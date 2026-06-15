e6edead136e8f33d0f80a8aa4f5209dc63378a48
uncle_joe <uncle_joe@kali.kali.com>
2026-06-15 14:01:07 +0300

    modified to provide support of fzf

--- fish/functions/j.fish
+++ fish/functions/j.fish
@@ -2,5 +2,10 @@
 #arguments: none
 #purpose: quickly change directory from list.
 function j
-  eval cd (cat ~/.config/jump.conf | fzf)
+    set dir (cat ~/.config/jump.conf | fzf < /dev/tty)
+    if test -n "$dir"
+        set dir (string replace -r '^~($|/)' "$HOME/" "$dir")
+        cd "$dir"
+        commandline -f repaint
+    end
 end
--- fish/functions/jc.fish
+++ fish/functions/jc.fish
@@ -2,5 +2,10 @@
 #arguments: none
 #purpose: quickly change directory from available at current directory.
 function jc
-  eval cd (ls | fzf)
+    set dir (ls | fzf < /dev/tty)
+    if test -n "$dir"
+        set dir (string replace -r '^~($|/)' "$HOME/" "$dir")
+        cd "$dir"
+        commandline -f repaint   # Refresh prompt after directory change
+    end
 end
