4d10bf5511ed079fa3139aa5a7d4823d9c61de46
uncle_joe <uncle_joe@kali.kali.com>
2026-06-15 15:33:15 +0300

    added shortcut for 'shutdown now'

---
+++ fish/functions/die.fish
@@ -0,0 +1,7 @@
+#function name: die
+#arguments: none
+#purpose: shortcut for shutdown now in flavour of my damned ol' Arch conf
+function die
+   echo "farewell, master of the jungle"
+   shutdown now
+end
