c913745c71e5d3d5c33232fcf1ae2026c702e0c7
uncle_joe <uncle_joe@kali.kali.com>
2026-07-02 17:44:20 +0300

    added fzf configs

--- fish/config.fish
+++ fish/config.fish
@@ -17,3 +17,11 @@ set -gx BROWSER firefox
 if status is-interactive
     fortune | cowsay
 end
+
+set -gx FZF_DEFAULT_OPTS "
+  --color=bg:#0a0a15,fg:#f8f8ff,hl:#6bd8ff
+  --color=bg+:#3a3a55,fg+:#ffffff,hl+:#ff9dff
+  --color=gutter:#0a0a15,pointer:#e7cfff,marker:#6bd8ff
+  --color=border:#3a3a55,info:#a8a8d0,prompt:#6bd8ff
+  --color=label:#a8a8d0,query:#f8f8ff,header:#a8a8d0
+"
