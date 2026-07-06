3e0a6619fd24d345dd7e1a96ba1c887ab1c1af8a
uncle_joe <uncle_joe@kali.kali.com>
2026-07-06 05:09:53 +0300

    use fzf, choose current task, set it to status bar

--- scripts/todo.fish
+++ scripts/todo.fish
@@ -4,6 +4,7 @@ set ktfs (kitten query-terminal font_size | tr -d -c 0-9)
 
 if test $ktfs = 10
    clear
-   batcat ~/docs/todos/current.org --style=grid
+   set ch_todo (batcat ~/docs/todos/current.org --style=grid | fzf)
+   fish ~/.config/scripts/set-todo.fish $ch_todo
 end   
 
