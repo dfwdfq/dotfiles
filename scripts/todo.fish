0e8a4a79d0525f48fd4ecac8f0d1cdc2e1e6a5d3
uncle_joe <uncle_joe@kali.kali.com>
2026-07-06 12:05:05 +0300

    print in exact order

--- scripts/todo.fish
+++ scripts/todo.fish
@@ -4,7 +4,7 @@ set ktfs (kitten query-terminal font_size | tr -d -c 0-9)
 
 if test $ktfs = 10
    clear
-   set ch_todo (batcat ~/docs/todos/current.org --style=grid | fzf)
+   set ch_todo (cat ~/docs/todos/current.org | fzf --tac)
    fish ~/.config/scripts/set-todo.fish $ch_todo
 end   
 
