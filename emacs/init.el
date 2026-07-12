63b15617886237f59c7c1b221f81a3ac54b54834
uncle_joe <uncle_joe@kali.kali.com>
2026-07-12 06:44:45 +0300

    added fzf

--- emacs/init.el
+++ emacs/init.el
@@ -17,6 +17,7 @@
 
 
 (add-to-list 'custom-theme-load-path "~/.config/emacs/themes/")
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (load-theme 'deep-purple t)
 
 (custom-set-variables
@@ -24,7 +25,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(rust-mode auto-complete corfu racket-mode)))
+ '(package-selected-packages '(fzf rust-mode auto-complete corfu racket-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
