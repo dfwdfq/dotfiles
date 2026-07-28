e30477f1107fc265208a19376a9dbc6a2d709592
uncle_joe <uncle_joe@kali.kali.com>
2026-07-29 02:18:10 +0300

    removed quotes

--- fish/functions/j.fish
+++ fish/functions/j.fish
@@ -3,9 +3,9 @@
 #purpose: quickly change directory from list.
 function j
     set dir (cat ~/.config/jump.conf | fzf --margin 20% --border --header "jump to..." --header-first --header-border inline)
-    if test -n "$dir"
+    if test -n $dir
         set dir (string replace -r '^~($|/)' "$HOME/" "$dir")
-        cd "$dir"
+        cd $dir
         commandline -f repaint
     end
 end
