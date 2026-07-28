bc7228cc4812d0745fce2fcc3318d5b784f0c442
uncle_joe <uncle_joe@kali.kali.com>
2026-07-28 19:04:20 +0300

    made it look better

--- fish/functions/j.fish
+++ fish/functions/j.fish
@@ -2,7 +2,7 @@
 #arguments: none
 #purpose: quickly change directory from list.
 function j
-    set dir (cat ~/.config/jump.conf | fzf < /dev/tty)
+    set dir (cat ~/.config/jump.conf | fzf --margin 20% --border --header "jump to..." --header-first --header-border inline)
     if test -n "$dir"
         set dir (string replace -r '^~($|/)' "$HOME/" "$dir")
         cd "$dir"
