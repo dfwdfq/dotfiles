f0663982c5e7fbcfe6348d92556a5d913fae622d
uncle_joe <uncle_joe@kali.kali.com>
2026-08-07 01:06:16 +0300

    updated a bit

--- emacs/lisp/c_stuff.el
+++ emacs/lisp/c_stuff.el
@@ -19,3 +19,21 @@
     (insert "#define " guard-var "\n\n")
     (insert "#endif /* " guard-var " */\n")
     (save-buffer)))
+
+(use-package company-c-headers
+  :ensure t
+  :after company)
+(defun my-c-autosuggest-setup ()
+  (set (make-local-variable 'company-backends)
+       '((company-c-headers   
+          company-clang       
+          company-keywords    
+          company-dabbrev-code)
+         company-dabbrev))) 
+
+
+(add-hook 'c-mode-hook #'my-c-autosuggest-setup)
+(add-hook 'c++-mode-hook #'my-c-autosuggest-setup)
+
+(setq company-idle-delay 0.1)          
+(setq company-minimum-prefix-length 2) 
