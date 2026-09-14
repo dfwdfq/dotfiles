c16dd84aa7a77d024a4cf3278d632431844315ea
uncle_joe <uncle_joe@kali.kali.com>
2026-09-14 09:26:41 +0300

    automated syncronisation of diary with encryption to codeberg

--- fish/functions/die.fish
+++ fish/functions/die.fish
@@ -2,6 +2,10 @@
 #arguments: none
 #purpose: shortcut for shutdown now in flavour of my damned ol' Arch conf
 function die
-   echo "farewell, master of the jungle"
-   shutdown now
+    cd ~/docs
+    git add *
+    git commit -m"update"
+    git push gcrypt::https://codeberg.org/dfwdfq/docs.git
+    echo "farewell, master of the jungle"
+    shutdown now
 end
