8e640dbd6f8217018d0e3edf5fe93cdb4963e997
uncle_joe <uncle_joe@kali.kali.com>
2026-09-19 09:53:39 +0300

    updated todo list update. Now finished task are moved away from both inbox and current list"

--- scripts/todo.fish
+++ scripts/todo.fish
@@ -3,7 +3,8 @@
 set ktfs (kitten query-terminal font_size | tr -d -c 0-9)
 
 if test $ktfs = 10
-   cat ~/docs/todos/current.org | perl ~/.config/scripts/update-current-todo.pl
+    cat ~/docs/todos/current.org | perl ~/.config/scripts/update-todo-list.pl current
+    cat ~/docs/todos/inbox.org   | perl ~/.config/scripts/update-todo-list.pl inbox
    clear   
    set ch_todo (cat ~/docs/todos/current.org | fzf --tac)
    fish ~/.config/scripts/set-todo.fish $ch_todo
