7fd746b28133ebcfe8db6d27fb42ff37dbd7aaef
uncle_joe <uncle_joe@kali.kali.com>
2026-09-21 23:58:57 +0300

    if file is choosen, print it and quit succesfully"

--- fish/functions/jd.fish
+++ fish/functions/jd.fish
@@ -1,4 +1,3 @@
-
 function jd
     set key -d
     if test (count $argv) -ge 1
@@ -7,6 +6,10 @@ function jd
     
     set opt (gdc $key | fzf --margin 10% --border $argv[2..-1])
     if test $status = 130
+	return 0
+    end
+    if test -f $opt
+	echo $opt
 	return 0
     end
     
