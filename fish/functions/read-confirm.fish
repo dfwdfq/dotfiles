f55c4d4c581fcdbd61ecca0a7ea741025ccb4b22
uncle_joe <uncle_joe@kali.kali.com>
2026-09-22 10:59:45 +0300

    common function to read confirmation from me

---
+++ fish/functions/read-confirm.fish
@@ -0,0 +1,12 @@
+function read-confirm
+  while true
+    read -l -P "$argv[1] [y/N] " confirm
+
+    switch $confirm
+      case Y y
+        return 0
+      case '' N n
+        return 1
+    end
+  end
+end
