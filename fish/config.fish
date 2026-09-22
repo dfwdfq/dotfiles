fcb6418a4e9ebdfdfe2f6f763f5e4f02bf4ca4c5
uncle_joe <uncle_joe@kali.kali.com>
2026-09-22 10:57:16 +0300

    added DM config

--- fish/config.fish
+++ fish/config.fish
@@ -32,3 +32,7 @@ set -gx FZF_DEFAULT_OPTS "
   --color=border:#3a3a55,info:#a8a8d0,prompt:#6bd8ff
   --color=label:#a8a8d0,query:#f8f8ff,header:#a8a8d0
 "
+
+set -gx DM_ROOT ~/docs
+set -gx DM_BASE_VIEW personal philosophy tech job idea
+set -gx DM_MISC_VIEW daily todos
