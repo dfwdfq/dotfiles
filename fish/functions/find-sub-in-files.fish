196219c1166e0b0fc72bf0e70b4141d78cdeeec6
uncle_joe <uncle_joe@kali.kali.com>
2026-09-22 10:57:38 +0300

    use fzf for searching

--- fish/functions/find-sub-in-files.fish
+++ fish/functions/find-sub-in-files.fish
@@ -1,22 +1,6 @@
 function find-sub-in-files
-    if test (count $argv) -ne 1
-	echo "fucked up!"
-	return 1
-    end
-
-    set bind_cmd 'ctrl-a:select-all,ctrl-d:deselect-all'
-    set header "find given substring in selected files"
-    set footer "TAB: select item, CTRL-a: select all, CTRL-d: deselect all"
-    
-    set sub $argv[1]    
-    set items (gdc -f | __fzf__ $header $footer --bind $bind_cmd --multi)
-    
-    for file in $items
-	printf "searching $sub in $file:\n"
-	grep -n $sub $file 
-	if test $status -eq 1
-	    printf "no $sub in $file.\n"
-	end
-	echo
-    end
+    grep -rnI --color=always . | fzf --ansi --delimiter : \
+	--preview 'batcat --color=always {1} --highlight-line {2}' \
+	--preview-window 'up:60%:wrap' \
+	--bind 'enter:become(emacs -n +{2} {1})'
 end
