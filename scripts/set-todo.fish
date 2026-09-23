292da1bc11f75bd32c645604a94031fc50873e23
uncle_joe <uncle_joe@kali.kali.com>
2026-09-23 10:45:07 +0300

    removed since they are replaced

--- scripts/set-todo.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-if test (count $argv) -lt 1
-    echo "Usage: set-todo.fish <current TODO>"
-    exit 1
-end
-
-echo $argv[1] > ~/.current_todo
-qtile cmd-obj -o widget TODO_TITLE -f update -a $argv[1]
